05c5639eb73f1c839717e28e585b4a688688c3d7
Jeffoz <thanw@yahoo.com>
2023-06-28 12:34:16 +1000

    Added our first SQL file

---
+++ car.sql
@@ -0,0 +1,12 @@
+CREATE TABLE car (
+make VARCHAR(100),
+model VARCHAR(100));
+
+INSERT INTO car(make, model) VALUES
+('Mitsubishi', 'Eclipse'),
+('Mitsubishi', 'Outlander'),
+('Hyundai', 'i30'),
+('Ram', '1500'),
+('Ford', 'F150'),
+('BMW', '335i'),
+('BMW', '330i');
